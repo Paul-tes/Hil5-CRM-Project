51dd3302082d8b3772d7cf847728ca58dbd67637
paul <86291500+Paul-tes@users.noreply.github.com>
2022-10-09 14:10:46 +0300

    Model Class in model folder introduced
    classes
    - Customers.c.s
    - Events.cs
    - Leads.cs
    - Organization.cs
    - Task.cs
    - Team.cs

---
+++ model/Customers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hil5_CRM_Project.model
+{
+    internal class Customers
+    {
+    }
+}
---
+++ model/Team.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hil5_CRM_Project.model
+{
+    internal class Team
+    {
+        public int id { get; set; }
+        public string picture { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string type { get; set; }
+        public string departement { get; set; }
+        public string role { get; set; }
+        public string status { get; set; }
+
+        public List<Leads> Teams { get; set; }
+        public List<Task> Tasks { get; set; }
+        public List<Events> Events { get; set; }
+        public List<Customers> Customers { get; set; }
+    }
+}
