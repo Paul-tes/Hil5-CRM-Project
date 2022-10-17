63aec28c9477e5fc99571346c68fa5ae9579441a
Kidus-y19 <kidusfesseha19@gmail.com>
2022-10-17 19:42:05 +0300

    Added a form AddEvents
    This form is used to add events by the team members

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace Hil5_CRM_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MdiMainForm());
+            Application.Run(new AddEvent());
         }
     }
 }
