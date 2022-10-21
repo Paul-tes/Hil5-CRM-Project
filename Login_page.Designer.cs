34d6843130e1d6ca1f3057e1de1093c479eb1026
Mckali-2k <kalid538598@gmail.com>
2022-10-21 09:45:57 +0300

    aDDED lOGIN PAGE
    Login page for both Admin and Employee, they have to provide their email and password

---
+++ Login_page.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace Hil5_CRM_Project
+{
+    partial class Login_page
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login_page));
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.btn_exit = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
+            this.gb_login = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.lbl_admin = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.cb_admin = new Guna.UI2.WinForms.Guna2ImageCheckBox();
+            this.btn_login = new Guna.UI2.WinForms.Guna2GradientTileButton();
+            this.lbl_forgot = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lbl_pswd = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.txt_pswd = new Guna.UI2.WinForms.Guna2TextBox();
+            this.lbl_email = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.txt_email = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Panel1.SuspendLayout();
+            this.guna2Panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.guna2Panel3.SuspendLayout();
+            this.gb_login.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.LightGray;
+            this.guna2Panel1.Controls.Add(this.btn_exit);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(990, 48);
+            this.guna2Panel1.TabIndex = 1;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_exit.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_exit.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_exit.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_exit.FillColor = System.Drawing.Color.DimGray;
+            this.btn_exit.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.ForeColor = System.Drawing.Color.White;
+            this.btn_exit.Location = new System.Drawing.Point(936, 3);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(51, 42);
+            this.btn_exit.TabIndex = 1;
+            this.btn_exit.Text = "X";
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.BackColor = System.Drawing.Color.GhostWhite;
+            this.guna2Panel2.Controls.Add(this.label1);
+            this.guna2Panel2.Controls.Add(this.guna2PictureBox1);
+            this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.guna2Panel2.Location = new System.Drawing.Point(0, 48);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(442, 437);
+            this.guna2Panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(42, 308);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(325, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Welcome to Hil5 CRM";
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Image = global::Hil5_CRM_Project.Properties.Resources.icon8_Hil5;
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(38, 37);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(338, 243);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox1.TabIndex = 3;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // guna2Panel3
+            // 
+            this.guna2Panel3.Controls.Add(this.gb_login);
+            this.guna2Panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2Panel3.Location = new System.Drawing.Point(442, 48);
+            this.guna2Panel3.Name = "guna2Panel3";
+            this.guna2Panel3.Size = new System.Drawing.Size(548, 437);
+            this.guna2Panel3.TabIndex = 3;
+            // 
+            // gb_login
+            // 
+            this.gb_login.Controls.Add(this.lbl_admin);
+            this.gb_login.Controls.Add(this.cb_admin);
+            this.gb_login.Controls.Add(this.btn_login);
+            this.gb_login.Controls.Add(this.lbl_forgot);
+            this.gb_login.Controls.Add(this.lbl_pswd);
+            this.gb_login.Controls.Add(this.txt_pswd);
+            this.gb_login.Controls.Add(this.lbl_email);
+            this.gb_login.Controls.Add(this.txt_email);
+            this.gb_login.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gb_login.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gb_login.ForeColor = System.Drawing.Color.Black;
+            this.gb_login.Location = new System.Drawing.Point(0, 0);
+            this.gb_login.Name = "gb_login";
+            this.gb_login.Size = new System.Drawing.Size(548, 437);
+            this.gb_login.TabIndex = 3;
+            this.gb_login.Text = "Please enter your information";
+            // 
+            // lbl_admin
+            // 
+            this.lbl_admin.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_admin.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_admin.Location = new System.Drawing.Point(460, 53);
+            this.lbl_admin.Name = "lbl_admin";
+            this.lbl_admin.Size = new System.Drawing.Size(54, 20);
+            this.lbl_admin.TabIndex = 8;
+            this.lbl_admin.Text = "ADMIN";
+            // 
+            // cb_admin
+            // 
+            this.cb_admin.CheckedState.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image")));
+            this.cb_admin.Image = ((System.Drawing.Image)(resources.GetObject("cb_admin.Image")));
+            this.cb_admin.ImageOffset = new System.Drawing.Point(0, 0);
+            this.cb_admin.ImageRotate = 0F;
+            this.cb_admin.Location = new System.Drawing.Point(426, 53);
+            this.cb_admin.Name = "cb_admin";
+            this.cb_admin.Size = new System.Drawing.Size(28, 20);
+            this.cb_admin.TabIndex = 7;
+            // 
+            // btn_login
+            // 
+            this.btn_login.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_login.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_login.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_login.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_login.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_login.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.ForeColor = System.Drawing.Color.White;
+            this.btn_login.Location = new System.Drawing.Point(21, 307);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(133, 42);
+            this.btn_login.TabIndex = 0;
+            this.btn_login.Text = "Login";
+            // 
+            // lbl_forgot
+            // 
+            this.lbl_forgot.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_forgot.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_forgot.ForeColor = System.Drawing.Color.Gray;
+            this.lbl_forgot.Location = new System.Drawing.Point(21, 253);
+            this.lbl_forgot.Name = "lbl_forgot";
+            this.lbl_forgot.Size = new System.Drawing.Size(149, 20);
+            this.lbl_forgot.TabIndex = 5;
+            this.lbl_forgot.Text = "Forgot my password";
+            this.lbl_forgot.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_pswd
+            // 
+            this.lbl_pswd.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_pswd.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_pswd.ForeColor = System.Drawing.Color.Black;
+            this.lbl_pswd.Location = new System.Drawing.Point(21, 179);
+            this.lbl_pswd.Name = "lbl_pswd";
+            this.lbl_pswd.Size = new System.Drawing.Size(99, 25);
+            this.lbl_pswd.TabIndex = 3;
+            this.lbl_pswd.Text = "Password:";
+            this.lbl_pswd.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt_pswd
+            // 
+            this.txt_pswd.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_pswd.DefaultText = "";
+            this.txt_pswd.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txt_pswd.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txt_pswd.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_pswd.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_pswd.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_pswd.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txt_pswd.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_pswd.Location = new System.Drawing.Point(21, 211);
+            this.txt_pswd.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_pswd.Name = "txt_pswd";
+            this.txt_pswd.PasswordChar = '●';
+            this.txt_pswd.PlaceholderText = "";
+            this.txt_pswd.SelectedText = "";
+            this.txt_pswd.Size = new System.Drawing.Size(491, 35);
+            this.txt_pswd.TabIndex = 2;
+            this.txt_pswd.UseSystemPasswordChar = true;
+            this.txt_pswd.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_pswd_KeyPress);
+            // 
+            // lbl_email
+            // 
+            this.lbl_email.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_email.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_email.ForeColor = System.Drawing.Color.Black;
+            this.lbl_email.Location = new System.Drawing.Point(21, 82);
+            this.lbl_email.Name = "lbl_email";
+            this.lbl_email.Size = new System.Drawing.Size(61, 25);
+            this.lbl_email.TabIndex = 1;
+            this.lbl_email.Text = "Email:";
+            this.lbl_email.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt_email
+            // 
+            this.txt_email.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_email.DefaultText = "";
+            this.txt_email.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txt_email.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txt_email.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_email.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_email.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_email.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txt_email.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_email.Location = new System.Drawing.Point(21, 114);
+            this.txt_email.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.PasswordChar = '\0';
+            this.txt_email.PlaceholderText = "";
+            this.txt_email.SelectedText = "";
+            this.txt_email.Size = new System.Drawing.Size(491, 35);
+            this.txt_email.TabIndex = 0;
+            // 
+            // Login_page
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 485);
+            this.Controls.Add(this.guna2Panel3);
+            this.Controls.Add(this.guna2Panel2);
+            this.Controls.Add(this.guna2Panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Login_page";
+            this.Text = "Login_page";
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel2.ResumeLayout(false);
+            this.guna2Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.guna2Panel3.ResumeLayout(false);
+            this.gb_login.ResumeLayout(false);
+            this.gb_login.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Button btn_exit;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
+        private Guna.UI2.WinForms.Guna2GroupBox gb_login;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lbl_admin;
+        private Guna.UI2.WinForms.Guna2ImageCheckBox cb_admin;
+        private Guna.UI2.WinForms.Guna2GradientTileButton btn_login;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lbl_forgot;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lbl_pswd;
+        private Guna.UI2.WinForms.Guna2TextBox txt_pswd;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lbl_email;
+        private Guna.UI2.WinForms.Guna2TextBox txt_email;
+    }
+}
